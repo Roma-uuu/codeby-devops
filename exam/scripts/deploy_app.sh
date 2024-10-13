7ad94c2bc363c33f455857715d03f6e4549fd1e3
Ramazan <romka1501@gmail.com>
2024-10-13 13:41:36 +0300

    Удалены ненужные файлы с использованием .gitignore

--- exam/scripts/deploy_app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Применить манифесты Kubernetes
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
-
-# Проверить статус Deployment и Pods
-kubectl get deployments
-kubectl get pods
--- exam/scripts/start_minikube.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Запуск Minikube с использованием драйвера Docker
-minikube start --driver=docker
-
-# Проверка состояния кластера
-minikube status
